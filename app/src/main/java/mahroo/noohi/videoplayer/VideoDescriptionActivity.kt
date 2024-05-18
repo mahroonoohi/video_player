0b23a5b826e31d095d188785aceb4d837c2e2ddf
Mahroo Noohi <mahroonoohi@gmail.com>
2024-05-18 11:02:51 -0800

    some MovieCard Added

--- app/src/main/java/mahroo/noohi/videoplayer/VideoDescriptionActivity.kt
+++ app/src/main/java/mahroo/noohi/videoplayer/VideoDescriptionActivity.kt
@@ -15,7 +15,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
@@ -75,7 +77,8 @@ class VideoDescriptionActivity : ComponentActivity() {
 @Composable
 fun MovieDetailPage() {
     val context = LocalContext.current
-    Column {
+    Column (modifier = Modifier
+        .verticalScroll(rememberScrollState())){
         SubcomposeAsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
                 .data(movie_image)
